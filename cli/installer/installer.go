e5ee672e5e5438287cd892cc8885758504bbedee
cyberb <support@syncloud.it>
2026-07-14 22:24:32 +0100

    Keep all Installer methods in installer.go (move InstallFreshRss/freshRssCli, drop freshrss.go)

--- cli/installer/installer.go
+++ cli/installer/installer.go
@@ -18,6 +18,8 @@ const (
 	CommonDir = "/var/snap/freshrss/common"
 )
 
+var seedFiles = []string{"config.custom.php", "config-user.custom.php", "opml.xml"}
+
 type Variables struct {
 	App             string
 	AppDir          string
@@ -250,4 +252,40 @@ func (i *Installer) RestorePreStart() error {
 
 func (i *Installer) RestorePostStart() error {
 	return i.Configure()
+}
+
+func (i *Installer) InstallFreshRss() error {
+	url, err := i.platformClient.GetAppUrl(App)
+	if err != nil {
+		return err
+	}
+
+	dataPath, err := i.DataPath()
+	if err != nil {
+		return err
+	}
+
+	for _, f := range seedFiles {
+		err = cp.Copy(path.Join(DataDir, "config", f), path.Join(dataPath, f))
+		if err != nil {
+			return err
+		}
+	}
+
+	_, err = i.freshRssCli(
+		"./cli/do-install.php",
+		"--default-user", "admin",
+		"--auth-type", "http_auth",
+		"--environment", "production",
+		"--base-url", url,
+		"--title", "Syncloud",
+		"--db-type", "sqlite",
+		"--api-enabled",
+	)
+	return err
+}
+
+func (i *Installer) freshRssCli(args ...string) (string, error) {
+	fullArgs := append([]string{"-E", "-u", App, path.Join(AppDir, "bin/freshrss.sh")}, args...)
+	return i.executor.Run("sudo", fullArgs...)
 }
